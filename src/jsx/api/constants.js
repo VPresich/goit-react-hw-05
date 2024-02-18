e401d0c79da3975453aa117b1857ddf8e93d809b
VPresich <valya.alexandrova.presich@gmail.com>
2024-02-18 09:56:13 +0100

    Update constants.js

--- src/jsx/api/constants.js
+++ src/jsx/api/constants.js
@@ -2,4 +2,4 @@ export const BASE_URL = 'https://api.themoviedb.org/3/';
 export const API_KEY = 'eee4cfb22166bee2478cc9bce5e0a15e';
 export const LOCAL_CONFIGURATION = 'tmdbConfig';
 
-export const BASE_IMG_URL = 'http://image.tmdb.org/t/p/';
+export const BASE_IMG_URL = 'https://image.tmdb.org/t/p/';
